3cf722af96c7ad93c4a445af442be9d59b62ae4b
xResto <kamil.korczak7215@gmail.com>
2024-02-20 20:07:08 +0100

    changed email for testing

--- app/actions/sendEmail.ts
+++ app/actions/sendEmail.ts
@@ -38,7 +38,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     await resend.emails.send({
       from: 'Formularz kontaktowy <onboarding@resend.dev>',
-      to: 'kamil.korczak7215@gmail.com',
+      to: 'krzyskor@op.pl',
       subject: 'Wiadomość z formularza kontaktowego',
       reply_to: senderEmail as string,
       react: React.createElement(ContactForm, {
