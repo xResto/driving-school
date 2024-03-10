4b718326c7371c5146103bda7dcd8345f79cd6ad
xResto <kamil.korczak7215@gmail.com>
2024-03-10 11:52:16 +0100

    Added dynamic photo blur.

--- app/galeria/Gallery.tsx
+++ app/galeria/Gallery.tsx
@@ -1,82 +1,31 @@
 import React from 'react';
 import Image from 'next/image';
-import Photo1 from '../../public/gallery/Peugeot1.webp';
-import Photo2 from '../../public/gallery/Peugeot2.webp';
-import Photo3 from '../../public/gallery/Peugeot3.webp';
-import Photo4 from '../../public/gallery/Peugeot4.webp';
-import Photo5 from '../../public/gallery/Peugeot5.webp';
-import Photo6 from '../../public/gallery/Peugeot6.webp';
-import Photo7 from '../../public/gallery/Peugeot7.webp';
-import Photo8 from '../../public/gallery/Peugeot8.webp';
-import MasonryWrapper from './MasonryWrapper';
 import { getPlaiceholder } from 'plaiceholder';
 import fs from 'node:fs/promises';
 
 interface GalleryProps {
   src: string;
+  width: number;
+  height: number;
   alt: string;
+  blurDataURL: string;
 }
 
-// niech to będzie tablica obiektów z src i alt
-const photoArray = [
-  Photo1,
-  Photo2,
-  Photo3,
-  Photo4,
-  Photo5,
-  Photo6,
-  Photo7,
-  Photo8,
-
-  Photo6,
-  Photo3,
-  Photo8,
-  Photo5,
-  Photo1,
-  Photo4,
-  Photo7,
-  Photo2,
-  Photo5,
-  Photo2,
-  Photo7,
-  Photo3,
-  Photo1,
-  Photo8,
-  Photo6,
-  Photo4,
-];
-
-const Gallery = ({ src, alt }: GalleryProps) => {
+const Gallery = ({ src, width, height, alt, blurDataURL }: GalleryProps) => {
   // const buffer = await fs.readFile(`./public/gallery/${}.webp`);
 
   return (
     <Image
       src={src}
       alt={alt}
-      width={1000}
-      height={1000}
+      width={width}
+      height={height}
       placeholder='blur'
+      blurDataURL={blurDataURL}
       // blurDataURL={await getPlaiceholder(src).then((res) => res.base64)}
       className='cursor-pointer rounded hover:opacity-75 active:opacity-75 hover:scale-105 transform transition-all'
     />
   );
-  
 };
 
 export default Gallery;
-
-// <MasonryWrapper>
-  // {photoArray.map((photo, index) => (
-  //   <Image
-  //     key={index}
-  //     src={photo.src}
-  //     alt={`Peugeot ${index + 1}`}
-  //     width={photo.width}
-  //     height={photo.height}
-  //     placeholder='blur'
-  //     // blurDataURL={photo.blurDataURL}
-  //     className='cursor-pointer rounded hover:opacity-75 active:opacity-75 hover:scale-105 transform transition-all'
-  //   />
-  // ))}
-
-  /* </MasonryWrapper> */
--- app/galeria/page.tsx
+++ app/galeria/page.tsx
@@ -21,34 +21,38 @@ const photoArray = [
   Photo6,
   Photo7,
   Photo8,
-
-  Photo6,
-  Photo3,
-  Photo8,
-  Photo5,
-  Photo1,
-  Photo4,
-  Photo7,
-  Photo2,
-  Photo5,
-  Photo2,
-  Photo7,
-  Photo3,
-  Photo1,
-  Photo8,
-  Photo6,
-  Photo4,
 ];
 
+async function calculateBase64() {
+  const base64Array = await Promise.all(
+    photoArray.map(async (element, index) => {
+      const buffer = await fs.readFile(
+        `./public/gallery/Peugeot${index + 1}.webp`
+      );
+      const { base64 } = await getPlaiceholder(buffer);
+      return base64;
+    })
+  );
+
+  return base64Array;
+}
+
 const Page = async () => {
-  const buffer = await fs.readFile(`./public/gallery/Peugeot1.webp`);
+  const base64Images = await calculateBase64();
 
   return (
     // <section className='max-w-[1200px] mx-auto xl:py-4 xl:px-0 min-[801px]:p-6 p-4 columns-1 sm:columns-2 min-[950px]:columns-3 xl:columns-4 gap-4'>
     <section className='max-w-[1200px] mx-auto xl:py-4 min-[1244px]:px-0 min-[801px]:p-6 p-4'>
       <MasonryWrapper>
         {photoArray.map((photo, index) => (
-          <Gallery key={index} src={photo.src} alt={`Peugeot ${index + 1}`} />
+          <Gallery
+            key={index}
+            src={photo.src}
+            width={photo.width}
+            height={photo.height}
+            alt={`Peugeot ${index + 1}`}
+            blurDataURL={base64Images[index]}
+          />
         ))}
       </MasonryWrapper>
     </section>
