c49bb65d4337bbb303365083d78abb72284da81b
xResto <kamil.korczak7215@gmail.com>
2024-03-13 20:59:35 +0100

    Added gallery modal with navigation.

--- app/galeria/Gallery.tsx
+++ app/galeria/Gallery.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Photo1 from '../../public/gallery/Peugeot1.webp';
 import Photo2 from '../../public/gallery/Peugeot2.webp';
@@ -9,8 +11,10 @@ import Photo6 from '../../public/gallery/Peugeot6.webp';
 import Photo7 from '../../public/gallery/Peugeot7.webp';
 import Photo8 from '../../public/gallery/Peugeot8.webp';
 import MasonryWrapper from './MasonryWrapper';
+import Modal from './Modal';
+import Link from 'next/link';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 
-// niech to będzie tablica obiektów z src i alt
 const photoArray = [
   {
     imgSrc: Photo1,
@@ -44,24 +48,104 @@ const photoArray = [
     imgSrc: Photo8,
     alt: 'Koperta na placu manwerowym WORD Tarnobrzeg.',
   },
+
+  {
+    imgSrc: Photo7,
+    alt: 'Samochód nauki jazdy znajdujący się na kopercie na placu manewrowym WORD Tarnobrzeg.',
+  },
+
+  {
+    imgSrc: Photo4,
+    alt: 'Wnętrze samochodu do nauki jazdy oraz plac manewrowy Tarnobrzeg.',
+  },
+  {
+    imgSrc: Photo5,
+    alt: 'Samochód nauki jazdy znajdujący się na kopercie na placu manewrowym WORD Tarnobrzeg.',
+  },
+  {
+    imgSrc: Photo6,
+    alt: 'Samochód nauki jazdy na placu manewrowym WORD Tarnobrzeg.',
+  },
+  {
+    imgSrc: Photo8,
+    alt: 'Koperta na placu manewrowym WORD Tarnobrzeg.',
+  },
+  {
+    imgSrc: Photo1,
+    alt: 'Samochód nauki jazdy na placu manewrowym.',
+  },
+  {
+    imgSrc: Photo2,
+    alt: 'Samochód nauki jazdy na placu manewrowym.',
+  },
+  {
+    imgSrc: Photo3,
+    alt: 'Samochód nauki jazdy na placu manewrowym.',
+  },
 ];
 
 const Gallery = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [currentImage, setCurrentImage] = useState({ src: '', alt: '' });
+  // const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const currentImageParam = searchParams.get('image');
+    if (currentImageParam) {
+      const currentImageIndex = parseInt(currentImageParam, 10) - 1;
+      setCurrentImage({
+        src: photoArray[currentImageIndex].imgSrc.src,
+        alt: photoArray[currentImageIndex].alt,
+      });
+      setIsOpen(true);
+    }
+  }, [searchParams]);
+
   return (
-    <MasonryWrapper>
-      {photoArray.map((photo, index) => (
+    <>
+      <MasonryWrapper>
+        {photoArray.map((photo, index) => (
+          <Link
+            href={`${pathname}?image=${index + 1}`}
+            scroll={false}
+            key={index}
+          >
+            <Image
+              src={photo.imgSrc.src}
+              alt={photo.alt}
+              width={photo.imgSrc.width}
+              height={photo.imgSrc.height}
+              placeholder='blur'
+              blurDataURL={photo.imgSrc.blurDataURL}
+              onClick={() => {
+                setIsOpen(true);
+                setCurrentImage({
+                  src: photo.imgSrc.src,
+                  alt: photo.alt,
+                });
+              }}
+              className='cursor-pointer rounded hover:opacity-75 active:opacity-75 hover:scale-105 transform transition-all'
+            />
+          </Link>
+        ))}
+      </MasonryWrapper>
+      <Modal
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        photoArrayLength={photoArray.length}
+      >
         <Image
-          key={index}
-          src={photo.imgSrc.src}
-          alt={photo.alt}
-          width={photo.imgSrc.width}
-          height={photo.imgSrc.height}
-          placeholder='blur'
-          blurDataURL={photo.imgSrc.blurDataURL}
-          className='cursor-pointer rounded hover:opacity-75 active:opacity-75 hover:scale-105 transform transition-all'
+          src={currentImage.src}
+          alt={currentImage.alt}
+          width={800}
+          height={600}
+          loading='lazy'
+          className='w-full h-auto'
         />
-      ))}
-    </MasonryWrapper>
+      </Modal>
+    </>
   );
 };
 
---
+++ app/galeria/Modal.tsx
@@ -0,0 +1,113 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import { Dialog } from '@headlessui/react';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+import Image from 'next/image';
+import CloseIcon from '../../public/close-icon.svg';
+import LeftArrowIcon from '../../public/left-icon.svg';
+import RightArrowIcon from '../../public/right-icon.svg';
+
+interface ModalProps {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  photoArrayLength: number;
+  children: React.ReactNode;
+}
+
+const Modal = ({
+  isOpen,
+  setIsOpen,
+  photoArrayLength,
+  children,
+}: ModalProps) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentImageParam = searchParams.get('image');
+
+  useEffect(() => {
+    currentImageParam
+      ? setCurrentImageIndex(parseInt(currentImageParam, 10))
+      : 0;
+  }, [searchParams]);
+
+  const handleLeftNavigation = () => {
+    const newImageParam = currentImageParam
+      ? parseInt(currentImageParam, 10) - 1
+      : 0;
+    router.push(`${pathname}?image=${newImageParam}`, { scroll: false });
+  };
+
+  const handleRightNavigation = () => {
+    if (currentImageIndex < photoArrayLength) {
+      const newImageParam = currentImageParam
+        ? parseInt(currentImageParam, 10) + 1
+        : 0;
+      router.push(`${pathname}?image=${newImageParam}`, { scroll: false });
+    } else {
+      return;
+    }
+  };
+
+  const isLeftArrowVisible = currentImageIndex !== 1;
+  const isRightArrowVisible = currentImageIndex !== photoArrayLength;
+
+  const handleClose = () => {
+    setIsOpen(false);
+    const params = new URLSearchParams(searchParams);
+    params.delete('image');
+    router.push(`${pathname}?${params.toString()}`, { scroll: false });
+  };
+
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={handleClose}
+      className='fixed inset-0 z-10 flex items-center justify-center p-4'
+    >
+      <div className='fixed inset-0 bg-black/75' aria-hidden='true' />
+      <Dialog.Panel className={`w-full flex items-center justify-between`}>
+        {isLeftArrowVisible && (
+          <button className='z-[100] text-white' onClick={handleLeftNavigation}>
+            <Image
+              src={LeftArrowIcon}
+              alt='Ikonka strzałki w lewo'
+              className='w-10 opacity-90 hover:scale-110 transform transition-all'
+            />
+          </button>
+        )}
+        {!isLeftArrowVisible && <div className='w-10'></div>}
+        <div className='transform overflow-hidden rounded-2xl p-2 text-left align-middle transition-all'>
+          <div className='flex flex-col'>
+            <button className='self-end' onClick={handleClose}>
+              <Image
+                src={CloseIcon}
+                alt='Ikonka X'
+                className='w-8 pb-2 hover:scale-110 transform transition-all opacity-90'
+              />
+            </button>
+            <div className='w-full box-border'>{children}</div>
+          </div>
+        </div>
+        {isRightArrowVisible && (
+          <button
+            className='z-[100] text-white'
+            onClick={handleRightNavigation}
+          >
+            <Image
+              src={RightArrowIcon}
+              alt='Ikonka strzałki w prawo'
+              className='w-10 opacity-90 hover:scale-110 transform transition-all'
+            />
+          </button>
+        )}
+        {!isRightArrowVisible && <div className='w-10'></div>}
+      </Dialog.Panel>
+    </Dialog>
+  );
+};
+
+export default Modal;
