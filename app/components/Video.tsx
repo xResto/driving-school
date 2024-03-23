f47634663309b9dad6a37a5cb81e1bebe660414f
xResto <kamil.korczak7215@gmail.com>
2024-03-23 16:10:41 +0100

    Adjusted video text responsiveness.

--- app/components/Video.tsx
+++ app/components/Video.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import DivOpacityAnimation from './animations/DivOpacityAnimation';
 
 const Video = () => {
   return (
@@ -6,8 +7,12 @@ const Video = () => {
       <video muted loop autoPlay disablePictureInPicture>
         <source src='/driving-cut.mp4' type='video/mp4' />
       </video>
-      <div className='absolute inset-0 flex justify-center items-center bg-black bg-opacity-50'>
-        <h4 className='text-white p-4 text-4xl bg-black/50 rounded-2xl'>Osiągnij swój cel - zdaj prawo jazdy z nami!</h4>
+      <div className='absolute inset-0 flex justify-center items-center bg-black/50 p-4'>
+        <DivOpacityAnimation duration={0.75}>
+          <h4 className='text-white text-center sm:p-4 min-[384px]:p-3 p-2 lg:text-4xl md:text-3xl sm:text-2xl min-[425px]:text-xl text-lg bg-black/50 rounded-2xl'>
+            Osiągnij swój cel - zdaj prawo jazdy z nami!
+          </h4>
+        </DivOpacityAnimation>
       </div>
     </div>
   );
